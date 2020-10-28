7a82d4d91e09fdd739e2e0edc49f7cbfa0c809da
Aland Andres Aguirre Mendoza <aland.a.mendoza@ntnu.no>
2020-10-28 16:33:59 +0100

    Commit with solution files

---
+++ task1/plus.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+read -p "Please enter first number: " num1
+read -p "Please enter second number: " num2
+echo "The total is: " $((num1+num2))
---
+++ task2/folderCounter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+folder_to_count=/web
+file_count=$(ls $folder_to_count | wc -l)
+echo $file_count files in $folder_to_count
+
