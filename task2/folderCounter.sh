5e1dd736411b5dbc1da5feb9cd7af0ace6192eda
Aland Andres Aguirre Mendoza <aland.a.mendoza@ntnu.no>
2020-10-28 22:36:28 +0100

    Added task solution for /web

--- task2/folderCounter.sh
+++ task2/folderCounter.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-folder_to_count=/web
+folder_to_count=/var
+folder_to_count2=/web
 file_count=$(ls $folder_to_count | wc -l)
+file_count2=$(ls $folder_to_count2 | wc -l)
 echo $file_count files in $folder_to_count
+echo $file_count2 files in $folder_to_count2
 
